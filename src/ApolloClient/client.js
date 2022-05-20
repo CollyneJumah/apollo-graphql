e04657cf37e8ca9cf8886b447b34c9688168d77e
CollyneJumah <collynesjumah2010@gmail.com>
2022-05-20 05:00:48 +0300

    added Apollo client

---
+++ src/ApolloClient/client.js
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from "@apollo/client";
+
+const httpLink = new HttpLink({
+    uri: "https://48p1r2roz4.sse.codesandbox.io"
+})
+
+export const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.from([httpLink])
+
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {client} from "./ApolloClient/client"
+import { ApolloProvider } from '@apollo/client';
+import ExchangeRatesPage from './ExchangeRatesPage';
+
 
 function App() {
   return (
+  <ApolloProvider client={client}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>This is React Apollo GraphQL Connection</h3>
       </header>
+      <ExchangeRatesPage />
     </div>
+  </ApolloProvider>
   );
 }
 
